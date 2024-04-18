6875cbc11b605a59a6170b284711c7ccd20d0ac2
Muhammadali1029 <104990048+Muhammadali1029@users.noreply.github.com>
2024-04-18 19:00:20 +0100

    implemented Update, delete and get operations from users. added token and admin verification for update and delete

---
+++ server/controllers/user.controller.js
@@ -0,0 +1,101 @@
+const { response } = require("express");
+const User = require("../models/user.model.js");
+
+const updateUser = async (req, res) => {
+    try {
+        const updatedUser = await User.findByIdAndUpdate(req.params.id, {
+            $set: req.body,
+        },{
+            new: true,
+        });
+        if (!updatedUser){
+            return res.status(404).json({
+                message: "User not found",
+            });
+        };
+        res.status(200).json({
+            message: "User has been updated successfully",
+            data: updatedUser,
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: "User Update failed",
+            error: error,
+        });
+    }
+}; 
+
+const deleteUser = async (req, res) => {
+    try {
+        await User.findByIdAndDelete(req.params.id);
+        res.status(200).json({
+            message: "User has been deleted successfully",
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: "User deletion failed",
+        });
+    }
+};
+
+const getAdmin = async (req, res) => {
+    try {
+        const admin = await User.findById(req.params.id);
+        if (!admin){
+            return res.status(404).json({
+                message: "User cant be found",
+            });
+        }
+        
+        const {password, ...info} = admin._doc;
+        res.status(200).json({
+            message: "User has been found successfully",
+            data: info,
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: "User query failed",
+            error: error,
+        });
+    }
+};
+
+const getAllUsers = async (req, res) => {
+    try {
+        const users = await User.find();
+        // Added a check for empty users array
+        if (!users || users.length === 0) {
+            return res.status(404).json({
+                message: "Users not found",
+            });
+        }
+        
+        // Using map to iterate over users array and remove passwords from each user object
+        const data = users.map(user => {
+            // Destructuring password and rest of the properties from user._doc
+            const { password, ...info } = user._doc;
+            return info; // Returning only the info object without password
+        });
+
+        res.status(200).json({
+            message: "All Users have been found successfully",
+            data: data,
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: "User query failed",
+            error: error.message, // Changed to only return error message
+        });
+    }
+};
+
+module.exports = { 
+    updateUser,
+    deleteUser,
+    getAdmin,
+    getAllUsers,
+ };
--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,7 +1,16 @@
+const User = require("../models/user.model.js");
+const { verifyToken, verifyAdmin } = require("../middleware/verifyToken.js");
+const { updateUser, deleteUser, getAdmin, getAllUsers } = require("../controllers/user.controller.js");
+
 const router = require("express").Router();
 
 router.get("/get_users", (req, res) => {
     res.send("All users retrieved");
 })
+
+router.put("/update/:id", verifyToken, updateUser);
+router.delete("/delete/:id", verifyAdmin, deleteUser);
+router.get("/getAdmin/:id", verifyAdmin, getAdmin);
+router.get("/", verifyToken, getAllUsers);
 
 module.exports = router;
